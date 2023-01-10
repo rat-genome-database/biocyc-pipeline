16686414e229ad847dfa51a3a6fbbdc80443ca15
tutajm <mtutaj@mcw.edu>
2023-01-10 13:30:02 -0600

    updated jar dependencies

--- src/main/dist/run.sh
+++ src/main/dist/run.sh
@@ -15,5 +15,5 @@ java -Dspring.config=$APPDIR/../properties/default_db2.xml \
     -Dlog4j.configurationFile=file://$APPDIR/properties/log4j2.xml \
     -jar lib/${APPNAME}.jar "$@" 2>&1 > $APPDIR/run.log
 
-mailx -s "[$SERVER] BioCyc pipeline run" $EMAIL_LIST < $APPDIR/logs/summary.log
+mailx -s "[$SERVER] BioCyc pipeline OK" $EMAIL_LIST < $APPDIR/logs/summary.log
 
