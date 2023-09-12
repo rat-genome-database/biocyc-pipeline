5181933dea91d808700d880eca80733b507037b8
llamersmcw <llamers@mcw.edu>
2023-09-12 11:22:05 -0500

    fixing issue with new data

--- src/main/java/edu/mcw/rgd/biocyc/Main.java
+++ src/main/java/edu/mcw/rgd/biocyc/Main.java
@@ -73,15 +73,16 @@ public class Main {
         String line;
         while( (line=in.readLine())!=null ) {
             String[] cols = line.split("[\\t]", -1);
-            
+
             // 1. The gene's RatCyc ID
             // 2. The gene's RGD ID (number, no prefix)
             // 3. The gene's NCBI ID (number, no prefix)
             // 4. The Uniprot ID of a product of the gene
             // 5. The RatCyc ID of a pathway associated with the gene
-            // 6. A URL that points to the RatCyc pathway Page for the pathway in column 5
-            // 7. A URL that points to the RatCyc gene page for the gene in column 1
-            // 8. (new) A URL that points to a gif image of the pathway diagram for the pathway in column 5
+            // 6. Name of pathway associated with the gene
+            // 7. A URL that points to the RatCyc pathway Page for the pathway in column 5
+            // 8. A URL that points to the RatCyc gene page for the gene in column 1
+            // 9. (new) A URL that points to a gif image of the pathway diagram for the pathway in column 5
 
 
             BioCycRecord r = new BioCycRecord();
@@ -89,9 +90,11 @@ public class Main {
             r.setGeneNcbiId(cols[2]);
             r.setUniProtId(cols[3]);
             r.setPathwayRatCycId(cols[4]);
-            r.setPathwayRatCycPage(cols[5]);
-            r.setGeneRatCycPage(cols[6]);
-            r.setPathwayRatCycImage(cols[7]);
+            r.setPathwayRatCycName(cols[5]);
+            r.setPathwayRatCycPage(cols[6]);
+            r.setGeneRatCycPage(cols[7]);
+            if (!r.getPathwayRatCycId().isEmpty())
+                r.setPathwayRatCycImage(cols[8]);
 
             if( !Utils.isStringEmpty(cols[1]) ) {
                 r.setGeneRgdId(Integer.parseInt(cols[1]));
